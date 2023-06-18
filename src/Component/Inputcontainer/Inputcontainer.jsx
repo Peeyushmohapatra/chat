cf527d82f6cfe2bdf001d05f0f90926463c89791
Peeyushmohapatra <peeyushm1998@gmail.com>
2023-06-18 21:30:20 +0530

    Task Done

--- src/Component/Inputcontainer/Inputcontainer.jsx
+++ src/Component/Inputcontainer/Inputcontainer.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Inputcontainer.css"
 import Input from '../Input/Input'
 
 const Inputcontainer = ({setDisplay,setUsers,selectedUser}) => {
+  const [bold,setBold] = useState("'Geologica', sans-serif");
+  const [italic,setItalic] = useState("'Exo 2', sans-serif");
   return (
     <div className='inputContainer'>
         <div className="topContainer">
         <i class="fa-solid fa-bold"></i>
         <i class="fa-sharp fa-solid fa-italic"></i>
         </div>
-        <div className="bottomContainer">
+        <div style={{fontFamily:italic}} className="bottomContainer">
+          hello
             <Input setDisplay={setDisplay} setUsers={setUsers} selectedUser={selectedUser}/>
         </div>
     </div>
